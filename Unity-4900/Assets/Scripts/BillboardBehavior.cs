070ab4e7a9d598086b9815c0978f9a0850b8670e
kate <ktolguin@gmail.com>
2019-09-15 15:19:09 -0400

    Pretty Grass

---
+++ Unity-4900/Assets/Scripts/BillboardBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var fwd = Camera.main.transform.forward;
+        fwd.y = -270;
+        transform.rotation = Quaternion.LookRotation(fwd);
+    }
+}
