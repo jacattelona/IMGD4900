51d4ee6161a03c69336e5089e611b7a47d486e10
Unknown <jacattelona@wpi.edu>
2019-09-16 00:06:25 -0400

    Tree and Melody
    Dancing Tree on picking a specific drum loop
    Looping melody and drums

--- Unity-4900/Assets/Scripts/Prototype Scripts/SoundManager.cs
+++ Unity-4900/Assets/Scripts/Prototype Scripts/SoundManager.cs
@@ -12,6 +12,7 @@ public class SoundManager : MonoBehaviour
 
     public AudioMixerGroup[] eqs;
 
+    public TreeManager tree;
 
     void Update()
     {
@@ -31,7 +32,7 @@ public class SoundManager : MonoBehaviour
 
         if (audioChoice != 0)
         {
-            if (drums.GetPlaying() && audioChoice < 4)
+            if (!drums.GetPlaying() && audioChoice < 4)
             {
                 drums.StartAll();
                 leads.StartAll();
@@ -44,6 +45,10 @@ public class SoundManager : MonoBehaviour
                 if (audioChoice < 4)
                 {
                     drums.UnMute(audioChoice - 1);
+                    if (audioChoice == 2)
+                        tree.StartDancing();
+                    else if (audioChoice != 2)
+                        tree.StopDancing();
                 }
 
                 else
@@ -52,18 +57,32 @@ public class SoundManager : MonoBehaviour
                 }
 
             }
+
+
         }
     }
 
     /// <summary>
-    /// 
+    /// Sets the chorus depth of this sound group
     /// </summary>
-    /// <param name="val"></param>
+    /// <param name="val"> value between 0 and 1 </param>
     public void SetChorus(float val)
     {
         if (val > 1.0f) val = 1.0f;
         if (val < 0f) val = 0;
         //chorus.depth = val / 2f;
         mixer.SetFloat("DepthLevel", val / 2f);
+    }
+
+
+    /// <summary>
+    /// Sets the reverb room size for this sound group
+    /// </summary>
+    /// <param name="val"> value between 0 and 1 </param>
+    public void SetReverb(float val)
+    {
+        if (val > 1.0f) val = 1.0f;
+        if (val < 0) val = 0;
+        mixer.SetFloat("RoomLevel", (val * 2500f) - 3000f);
     }
 }
---
+++ Unity-4900/Assets/Scripts/Prototype Scripts/TreeManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeManager : MonoBehaviour
+{
+    int beat = 33;
+    int beatCount = 0;
+    int count = 0;
+    public enum TreeState
+    {
+        Nothing,
+        Waiting,
+        Dancing
+    }
+
+    Animator anim;
+    TreeState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        state = TreeState.Nothing;
+    }
+
+    public void StartDancing()
+    {
+        //anim.SetBool("CorrectLoop", true);
+        state = TreeState.Waiting;
+    }
+
+    public void StopDancing()
+    {
+        state = TreeState.Nothing;
+        anim.SetBool("CorrectLoop", false);
+    }
+
+    void FixedUpdate()
+    {
+        count++;
+
+        if (beatCount == 2)
+        {
+            if (count == 34)
+            {
+                count = 0;
+                beatCount = 0;
+            }
+        }
+
+        else
+        {
+            if (count == 33)
+            {
+                count = 0;
+                beatCount++;
+
+            }
+        }
+
+        if (count == 0)
+            print("Beat");
+        if (state == TreeState.Waiting && count == 0)
+        {
+            //print("GO");
+            anim.SetBool("CorrectLoop", true);
+            state = TreeState.Dancing;
+        }
+
+    }
+
+    
+
+}
