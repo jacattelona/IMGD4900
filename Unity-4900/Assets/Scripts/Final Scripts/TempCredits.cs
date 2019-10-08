8102914bc544e3183bc43277173dc71c47da37bc
Unknown <jacattelona@wpi.edu>
2019-10-08 09:51:05 -0400

    Temporary Credits Solution

---
+++ Unity-4900/Assets/Scripts/Final Scripts/TempCredits.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempCredits : MonoBehaviour
+{
+    [SerializeField]
+    GameObject can;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        can.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            can.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            can.SetActive(false);
+        }
+    }
+}
